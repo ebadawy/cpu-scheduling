3a66cba8165805d032f0b25855171c07f4888b70
ehab93 <ehalbadawy93@gmail.com>
2015-04-06 04:22:56 +0200

    Implement queue datastructure

---
+++ javascripts/datastructure.js
@@ -0,0 +1,31 @@
+var Node = function(data) {
+	this.data = data;
+	this.next = null;
+}
+
+var Queue = function() {
+	this.first = null;
+	this.size = null;
+}
+
+Queue.prototype.enqueue = function(data) {
+	var node = new Node(data);
+
+	if(!this.first) {
+		this.first = node;
+	} else {
+		n = this.first;
+		while(n.next) 
+			n = n.next;
+		n.next = node;
+	}
+	this.size += 1;
+	return node;
+}
+
+Queue.prototype.dequeue = function() {
+	temp = this.first;
+	this.first = this.first.next;
+	this.size -= 1;
+	return temp;
+}
