88e9ccfec7b6020c6a717f3a0268ada5c1e07483
ehab93 <ehalbadawy93@gmail.com>
2015-04-07 05:56:44 +0200

    Update dropdown menu test when clicking on one of its items

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,11 @@
+var $scheduler_type_btn = $('button.scheduler-type');
+
+//update dropdown menu text when clicking on one of its items
+$('ul.scheduler-type li').click(function() {
+	var html = $(this).text().trim()+' <span class="caret"></span>';
+	$scheduler_type_btn.html(html);
+});
+
 // update inputs numbers to matche with processes numbers
 $(".processes-number").keydown(function(e){
     var keycode = (event.keyCode ? event.keyCode : event.which);
@@ -12,7 +20,7 @@ $(".processes-number").keydown(function(e){
 });
 
 $('#run-div').delegate('.btn.run', 'click', function() {
-	var scheduler_type = $('button.scheduler-type').text().trim();
+	var scheduler_type = $scheduler_type_btn.text().trim();
 	switch(scheduler_type) {
 		case 'FCFS': 
 			fcfs(); 
